9c8609aff29ce05bb23d2e7fa83b71b2117ae4d9
tinchovictory <martin@victory.com.ar>
2019-06-21 15:43:44 -0300

    decrypter working as in the paper

--- decrypter/decrypter.c
+++ decrypter/decrypter.c
@@ -2,24 +2,20 @@
 #include <stdlib.h>
 
 #include "decrypter.h"
+#include "../utils/gaussElimination/gaussElimination.h"
 
 /* Helper functions definitions */
-// static int is_shares_array_valid(const matrix_array_t shares, int k);
 static matrix_t build_matrix_B(const matrix_array_t shares);
 static int get_shares_height(const matrix_array_t shares);
 static matrix_array_t build_matrices_G(const matrix_array_t shares);
+static matrix_t build_matrix_R(const int sharesIdx[], const matrix_array_t arrayG);
 
 /*
  * Decrypter
  */
 void decrypt(const matrix_array_t shares, const int sharesIdx[], const matrix_t remainder) {
-  matrix_t matB = NULL, doubleS = NULL;
+  matrix_t matB = NULL, doubleS = NULL, matR = NULL, matS = NULL, matW = NULL;
   matrix_array_t arrayG = NULL;
-
-  /* Check shares size */
-  // if(!is_shares_array_valid(shares, k)) {
-  //   goto out;
-  // }
 
   /* Build matrix B */
   matB = build_matrix_B(shares);
@@ -35,6 +31,8 @@ void decrypt(const matrix_array_t shares, const int sharesIdx[], const matrix_t 
     goto out;
   }
 
+  // Free matB
+
   /* Build matrices G */
   arrayG = build_matrices_G(shares);
   if(arrayG == NULL) {
@@ -43,10 +41,22 @@ void decrypt(const matrix_array_t shares, const int sharesIdx[], const matrix_t 
   }
 
   /* Build remainder matrix R */
+  matR = build_matrix_R(sharesIdx, arrayG);
+  if(matR == NULL) {
+    goto out;
+  }
 
   /* Build secret matrix S */
+  matS = add_matrix(doubleS, matR);
+  if(matS == NULL) {
+    goto out;
+  }
 
   /* Build watermark */
+  matW = add_matrix(doubleS, remainder);
+  if(matW == NULL) {
+    goto out;
+  }
 
 
   // TEST
@@ -55,32 +65,25 @@ void decrypt(const matrix_array_t shares, const int sharesIdx[], const matrix_t 
   printf("Mat double S is \n");
   print_matrix(doubleS);
   print_matrix_array(arrayG, "G");
+  printf("Mat R is \n");
+  print_matrix(matR);
+  printf("Mat S is \n");
+  print_matrix(matS);
+  printf("Mat W is \n");
+  print_matrix(matW);
 
   out:
   free_matrix(matB);
   free_matrix(doubleS);
   free_matrix_array(arrayG);
+  free_matrix(matR);
+  free_matrix(matS);
+  free_matrix(matW);
 }
 
 
+
 /* Helper functions */
-
-// static int is_shares_array_valid(const matrix_array_t shares, int k) {
-//   int count = 0;
-
-//   if(shares == NULL) {
-//     return 0;
-//   }
-
-//   for(int i = 0; i < get_matrix_array_size(shares); i++) {
-//     if(get_matrix_array_item(shares, i) != NULL) {
-//       count++;
-//     }
-//   }
-
-//   return count == k;
-// }
-
 
 static matrix_t build_matrix_B(const matrix_array_t shares) {
   matrix_t matB, share;
@@ -160,4 +163,66 @@ static matrix_array_t build_matrices_G(const matrix_array_t shares) {
   }
 
   return arrayG;
+}
+
+
+static matrix_t build_matrix_R(const int sharesIdx[], const matrix_array_t arrayG) {
+  matrix_t matR, fShare, matEq, share, solvedEq;
+  int Rj;
+
+  /* Get first share to read dimensions */
+  fShare = get_matrix_array_item(arrayG, 0);
+  if(fShare == NULL) {
+    return NULL;
+  }
+
+  /* Allocate matR */
+  matR = new_matrix(get_matrix_height(fShare), get_matrix_array_size(arrayG) * get_matrix_width(fShare));
+  if(matR == NULL) {
+    return NULL;
+  }
+
+  /* Build equation matrix with the shares index */
+  matEq = new_matrix(get_matrix_array_size(arrayG), 3);
+  if(matEq == NULL) {
+    free_matrix(matR);
+    return NULL;
+  }
+
+  /* Fill equation matrix with shares index */
+  for(int i = 0; i < get_matrix_height(matEq); i++) {
+    set_matrix(matEq, i, 0, 1);
+    set_matrix(matEq, i, 1, sharesIdx[i]);
+  }
+
+  /* Iterate over the shares to fill R */
+  for(int i = 0; i < get_matrix_height(fShare); i++) {
+    for(int j = 0; j < get_matrix_width(fShare); j++) {
+      /* Iterate over the shares array */
+      for(int k = 0; k < get_matrix_array_size(arrayG); k++) {
+        share = get_matrix_array_item(arrayG, k);
+        set_matrix(matEq, k, 2, get_matrix(share, i, j));
+      }
+
+      /* Calculate gauss jordan elimination */
+      solvedEq = apply_gauss_elimination(matEq);
+      if(solvedEq == NULL) {
+        free_matrix(matR);
+        free_matrix(matEq);
+        return NULL;
+      }
+
+      /* Fill values to R */
+      for(int k = 0; k < get_matrix_height(solvedEq); k++) {
+        Rj = j * get_matrix_height(solvedEq) + k;
+        set_matrix(matR, i, Rj, get_matrix(solvedEq, k, 2));
+      }
+
+      free_matrix(solvedEq);
+    }
+  }
+
+  free_matrix(matEq);
+
+  return matR;
 }
